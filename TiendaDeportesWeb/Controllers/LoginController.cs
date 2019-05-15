d175b19f7988eb252644ab794d6031661ff5ba52
Alvaro Gutierrez <algut0511@gmail.com>
2019-05-14 20:44:46 -0500

    validacion de el login

--- TiendaDeportesWeb/Controllers/LoginController.cs
+++ TiendaDeportesWeb/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TiendaDeportesWeb.Models;
 
 namespace TiendaDeportesWeb.Controllers
 {
@@ -12,6 +13,29 @@ namespace TiendaDeportesWeb.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Enter (String user, string pwd)
+        {
+            try
+            {
+                using (tiendaEntities db = new tiendaEntities())
+                {
+                    var usuarios = from p in db.personas
+                                   where p.id_persona == int.Parse(user) && p.contrasena == pwd && p.tipo_persona == "emp"
+                                   select p;
+                    if (usuarios.Count()> 0)
+                    {
+                        personas oPersonas = usuarios.First();
+                        Session["user"] = oPersonas;
+                        return Content("1");
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                return Content("error :" + e.Message);
+            }
         }
     }
 }
